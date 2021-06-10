cdcfce7fde7953b348cbe616719b147dca9de68c
Lichess-Cruzer <alexhenige@gmail.com>
2021-06-09 21:07:17 -0700

    Wednesday Upload

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Card = ({
+  name,
+  type,
+  manaCost,
+  text,
+  flavor,
+  imageUrl
+}) => {
+  return (
+    <div className="Card">
+      { name } - { manaCost } <br />
+      { type } <br />
+      { text } <br />
+      { flavor } <br />
+      <img src={ imageUrl } />
+    </div>
+  );
+}
+
+export default Card;
+
